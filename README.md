6a01f843c42424752f33d34939304d597340d552
Abhishek Malik <a.malik@campus.lmu.de>
2021-01-06 17:07:56 +0100

    Improved documentation

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Exoplanet-Vetting-Tool [![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/abhmalik/Exoplanet-Vetting-Tool/master)
-The vetting tool is to help people explore results from our exoplanet detection model in an interactive way without any knowledge of the underlying machine learning methods and the feature calculation techniques.
+The vetting tool is to help people explore the results from our exoplanet detection model from the paper: [Exoplanet Detection using Machine Learning](https://arxiv.org/abs/2011.14135)
+With the tool you can explore some of the predicted planet cases in an interactive way without any knowledge of the underlying machine learning methods and the feature calculation techniques.
 
 ## How to run the tool:
 1. [open binder](https://mybinder.org/v2/gh/abhmalik/Exoplanet-Vetting-Tool/master) and run `Vetting-Tool.ipynb`
--- functions.py
+++ functions.py
@@ -1,27 +1,8 @@
 import pandas
 
-def show_global_view(lc, i):
-    # plt.figure(figsize=(20,10))
-    lc.plot()
-    plt.title(f'Global View: {i}', fontsize=16)
-    show_inline_matplotlib_plots()
-
-def show_local_view(lc, i):
-    # plt.figure(figsize=(15,10))
-    lc.fold(period=500).plot()
-    plt.title(f'Folded View: {i}', fontsize=16)
-    show_inline_matplotlib_plots()
-    
-def plot_shap_values(case_interest_lc, i):
-    plt.figure(figsize=(15,10))
-    # index = case_interest_lc.iloc[i].index
-    dd = predicted_planets_shaps.iloc[i][1:]
-    dd = dd.sort_values(ascending=False)
-    dd[:10].sort_values(ascending=True).plot(kind='barh')
-    plt.title(f'Shap Value Plot: {i}', fontsize=16)
-    show_inline_matplotlib_plots()
-
 def plot_selected(i):
+	'''Plotting transaction details i.e. Global/Local view or SHAP values based on dropdown value for selected case'''
+	
         clear_output()
         lc = lk.LightCurve(flux=case_interest_lc.iloc[i])
         if dropdown.value == 'Global View':
@@ -33,17 +14,51 @@ def plot_selected(i):
         if dropdown.value == 'SHAP Values':
             printmd(f'### SHAP Values for index: {i}')
             plot_shap_values(case_interest_lc, i)
+
+
+def show_global_view(lc, i):
+	'''Plotting Global View for selected trx'''
+
+    lc.plot()
+    plt.title(f'Global View: {i}', fontsize=16)
+    show_inline_matplotlib_plots()
+
+
+def show_local_view(lc, i):
+	'''Plotting Local View for selected trx'''
+
+    lc.fold(period=500).plot()
+    plt.title(f'Folded View: {i}', fontsize=16)
+    show_inline_matplotlib_plots()
+    
+
+def plot_shap_values(case_interest_lc, i):
+	'''Dispalying SHAP value plot for the concerned case'''
+
+    plt.figure(figsize=(15,10))
+    dd = predicted_planets_shaps.iloc[i][1:]
+    dd = dd.sort_values(ascending=False)
+    dd[:10].sort_values(ascending=True).plot(kind='barh')
+    plt.title(f'Shap Value Plot: {i}', fontsize=16)
+    show_inline_matplotlib_plots()
+
             
 def next_button_clicked(b):
+	'''change of global index when next button is toggles'''
+
     with out:
         global ix
         ix += 1
 
+
 def prev_button_clicked(b):
+	'''change of global index when prev button is toggles'''
+
     with out:
         global ix
         ix -= 1
         if ix<0: ix=0
+
         
 def refresh_button_clicked(b):
     with out:
